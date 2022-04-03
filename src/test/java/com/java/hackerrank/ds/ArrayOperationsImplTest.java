71e4a6f5db9e6cd2e03c2b0ba8693e691b8d6a43
vinod <vinod.naik06@gmail.com>
2022-04-03 09:40:55 +0100

    first file rename commit

--- src/test/java/com/java/hackerrank/ds/ArrayOperationsImplTest.java
+++ src/test/java/com/java/hackerrank/ds/ArrayOperationsImplTest.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class ReverseArrayImplTest {
+public class ArrayOperationsImplTest {
     ArrayOperations arrayOperations = new ArrayOperationsImpl();
 
     @Test
